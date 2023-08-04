1573307a4de051f24ebe67c2b0ff9adee0bb25d2
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-08-04 19:48:42 +0000

    Update: retry

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,13 +5,10 @@
 import urllib.request
 
 
-if __name__ == "__main__":
-    '''Something here
-    '''
-    url = 'https://alu-intranet.hbtn.io/status'
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        data = response.read()
-        info = f"Body response:\n\t- type: {type(data)}\n\t- \
-content: {data}\n\t- utf8 content: {data.decode('utf-8')}"
-        print(info)
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
