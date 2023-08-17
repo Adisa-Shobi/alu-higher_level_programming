55794cc7bdf7da2ed3183dd8f39d97f9813e3ca2
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-08-17 19:16:16 +0000

    Update: correction

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State):
-        if 'a' in state.name:
+        if 'a' in state.name or 'A' in state.name:
             session.delete(state)
     session.commit()
     session.close()
