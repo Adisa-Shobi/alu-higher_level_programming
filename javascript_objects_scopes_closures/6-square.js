2cbc83a6af95ec0af32f5157938eb69368fcf6f5
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-09-30 11:31:35 +0000

    Update: bug fix

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./5-square.js');
+const SquareM = require('./5-square.js');
 
-class Square extends Square {
+class Square extends SquareM {
   constructor (size) {
     super(size, size);
   }
