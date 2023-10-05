fb38db5c70bdf5b49b642bdd4422c5ae25d51f18
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-10-05 11:06:56 +0000

    Create: majority task complete

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const charId = 18;
+const url = process.argv[2];
+const charUrl = url.replace('films', `people/${charId}/`);
+let count = 0;
+
+request.get(url, (err, resp, body) => {
+  if (err) {
+    throw err;
+  }
+  body = JSON.parse(body);
+  body.results.forEach((film, index) => {
+    if (film.characters.includes(charUrl)) {
+      count++;
+    }
+  });
+  console.log(count);
+});
