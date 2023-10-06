f9801625ee2f437cac24bbca046c5548bd04b784
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-10-06 18:01:06 +0000

    Update: modified

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const charId = 18;
-const url = process.argv[2];
-const charUrl = url.replace('films', `people/${charId}/`);
-let count = 0;
 
-request.get(url, (err, resp, body) => {
-  if (err) {
-    throw err;
-  }
-  body = JSON.parse(body);
-  body.results.forEach((film, index) => {
-    film.characters.forEach((character, index) => {
-      if (character === charUrl) {
-        count++;
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const movieCount = data.results.length;
+    let finalResult = 0;
+    // const comString = 'https://swapi-api.hbtn.io/api/people/18/';
+    for (let i = 0; i < movieCount; i++) {
+      const numChars = data.results[i].characters.length;
+      for (let j = 0; j < numChars; j++) {
+        const comString0 = data.results[i].characters[j];
+        if (comString0.slice(-4, -1) === '/18') {
+          finalResult += 1;
+        }
       }
-    });
-  });
-  console.log(count);
+    }
+    console.log(finalResult);
+  }
 });
